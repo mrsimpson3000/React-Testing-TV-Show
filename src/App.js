a3f6d99ed0e139919364a3210fd1fc5ca05fbc54
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-20 15:36:41 -0400

    Created an api directory and fetchShow.js inside said directoy. The useEffect hook is still in the App.js folder and works as it should.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import parse from "html-react-parser";
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
+import { fetchShow } from "./api/fetchShow";
 import "./styles.css";
 
 export default function App() {
@@ -15,20 +16,13 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow().then((res) => {
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    });
   }, []);
 
-  const handleSelect = e => {
+  const handleSelect = (e) => {
     setSelectedSeason(e.value);
   };
 
@@ -37,15 +31,15 @@ export default function App() {
   }
 
   return (
-    <div className="App">
-      <img className="poster-img" src={show.image.original} alt={show.name} />
+    <div className='App'>
+      <img className='poster-img' src={show.image.original} alt={show.name} />
       <h1>{show.name}</h1>
       {parse(show.summary)}
       <Dropdown
         options={Object.keys(seasons)}
         onChange={handleSelect}
         value={selectedSeason || "Select a season"}
-        placeholder="Select an option"
+        placeholder='Select an option'
       />
       <Episodes episodes={episodes} />
     </div>
